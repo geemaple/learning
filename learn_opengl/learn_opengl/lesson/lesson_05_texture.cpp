e54840964101a67b945bc8ff568604da3cfaf5af
Felix Ji <GeeMaple@gmail.com>
2023-01-08 17:04:19 +0800

    combine two texture

--- learn_opengl/learn_opengl/lesson/lesson_05_texture.cpp
+++ learn_opengl/learn_opengl/lesson/lesson_05_texture.cpp
@@ -21,18 +21,15 @@ int Lesson05::entry(void) {
         0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f,  0.5f, 1.0f  // top
     };
     
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load("wall.jpg", &width, &height, &nrChannels, 0);
-    
-    GLuint VBO, VAO, texture;
+    GLuint VBO, VAO, texture[2];
     // create
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenTextures(1, &texture);
+    glGenTextures(2, texture);
     // bind
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    
     // vetex
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
@@ -42,10 +39,34 @@ int Lesson05::entry(void) {
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
     glEnableVertexAttribArray(2);
-    // texture
-    if (data) {
+    
+    stbi_set_flip_vertically_on_load(true);
+    // texture unit 0
+    {
+        int width, height, nrChannels;
+        unsigned char *data = stbi_load("wall.jpg", &width, &height, &nrChannels, 0);
+        
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture[0]);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
+        stbi_image_free(data);
+        
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+    
+    // texture unit 1
+    {
+        int width, height, nrChannels;
+        unsigned char *data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture[1]);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        stbi_image_free(data);
         
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -60,6 +81,11 @@ int Lesson05::entry(void) {
     GLenum types[] = {GL_VERTEX_SHADER, GL_FRAGMENT_SHADER};
     const char* paths[] = {"lesson5_vertex.glsl", "lesson5_frament.glsl"};
     GLuint shaderProgram = shaderProgramFromFile(types, paths, 2);
+    
+    // set uniforms
+    glUseProgram(shaderProgram);
+    glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0);
+    glUniform1i(glGetUniformLocation(shaderProgram, "texture2"), 1);
     
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     // render loop, each iteration is called a frame
@@ -82,6 +108,7 @@ int Lesson05::entry(void) {
     // clean up all the resources
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteTextures(2, texture);
     glDeleteProgram(shaderProgram);
     glfwTerminate();
     
