c5525e439025eef853dcead15c845fa4cbeb17a6
Felix <geemaple@gmail.com>
2024-05-22 12:19:32 +0800

    add project as submodule

--- learn_cs/cs162_operating_system/run.sh
+++ learn_cs/cs162_operating_system/run.sh
@@ -1,2 +1,4 @@
-git clone https://github.com/PKU-OS/pintos.git
-docker run -it --rm --name pintos --mount type=bind,source=$HOME/Developer/learning/learn_cs/cs162_operating_system/pintos,target=/home/PKUOS/pintos pkuflyingpig/pintos bash
+
+pintos_path="$HOME/Developer/learning/learn_cs/cs162_operating_system/pintos"
+git submodule update --init pintos
+docker run -it --rm --name pintos --mount type=bind,source=$pintos_path,target=/home/PKUOS/pintos pkuflyingpig/pintos bash
