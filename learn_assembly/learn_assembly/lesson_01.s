65d3fd547732fde36babf82b51ba993ad427feb8
Felix <geemaple@gmail.com>
2024-05-23 15:07:13 +0800

    update 兼容

--- learn_assembly/learn_assembly/lesson_01.s
+++ learn_assembly/learn_assembly/lesson_01.s
@@ -5,17 +5,20 @@
 //  Created by Felix on 2024/5/23.
 //
 
-.section __TEXT,__text,regular,pure_instructions
-.globl _asm_main
-
-_asm_main:
-    movl $42, %eax    # 将42放入eax寄存器，作为返回值
-    ret               # 返回
-
-
+#if defined(__arm64__)
 .section __TEXT,__text
 .globl _asm_main
 
 _asm_main:
-    mov x0, #42    // 将42放入 x0 寄存器，作为返回值
+    mov x0, #43    // 将42放入 x0 寄存器，作为返回值
     ret            // 返回
+#else
+.section __TEXT,__text,regular,pure_instructions
+.globl _asm_main
+
+_asm_main:
+    movl $40, %eax    # 将42放入eax寄存器，作为返回值
+    ret               # 返回
+#endif
+
+
